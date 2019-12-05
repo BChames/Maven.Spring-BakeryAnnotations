5118a1c06dde584ea47a08c49c9cbbedf1c432f8
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-05 16:50:28 -0500

    All test passing

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BakerController {
@@ -17,24 +15,24 @@ public class BakerController {
     public BakerController(BakerService service) {
         this.service = service;
     }
-    @GetMapping("/baker")
+    @GetMapping("/bakers")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-    @GetMapping("/baker/{id}")
-    public ResponseEntity<Baker> show(Long id) {
+    @GetMapping("/bakers/{id}")
+    public ResponseEntity<Baker> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-    @PutMapping("/baker")
-    public ResponseEntity<Baker> create(Baker baker) {
+    @PostMapping("/bakers")
+    public ResponseEntity<Baker> create(@RequestBody Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-    @PutMapping("/baker/{id}")
-    public ResponseEntity<Baker> update(Long id, Baker baker) {
+    @PutMapping("/bakers/{id}")
+    public ResponseEntity<Baker> update(@PathVariable Long id, @RequestBody Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-    @DeleteMapping("/baker/{id}")
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @DeleteMapping("/bakers/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -16,25 +16,25 @@ public class MuffinController {
     public MuffinController(MuffinService service) {
         this.service = service;
     }
-    @GetMapping("/muffin")
+    @GetMapping("/muffins")
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-    @GetMapping("/muffin/{id}")
-    public ResponseEntity<Muffin> show(Long id) {
+    @GetMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-    @PutMapping("/muffin")
-    public ResponseEntity<Muffin> create(Muffin baker) {
+    @PostMapping("/muffins")
+    public ResponseEntity<Muffin> create(@RequestBody Muffin baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
-    @PutMapping("/muffin/{id}")
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @PutMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> update(@PathVariable Long id, @RequestBody Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-    @DeleteMapping("/muffin/{id}")
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @DeleteMapping("/muffins/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
