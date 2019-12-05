58407852da5eb3f4969c062cb5c4c822ebd93a1b
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-05 16:08:59 -0500

    Complete postman

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -2,32 +2,38 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
+@Controller
 public class BakerController {
     private BakerService service;
 
     public BakerController(BakerService service) {
         this.service = service;
     }
-
+    @GetMapping("/baker")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-
+    @GetMapping("/baker/{id}")
     public ResponseEntity<Baker> show(Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-
+    @PutMapping("/baker")
     public ResponseEntity<Baker> create(Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-
+    @PutMapping("/baker/{id}")
     public ResponseEntity<Baker> update(Long id, Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-
+    @DeleteMapping("/baker/{id}")
     public ResponseEntity<Boolean> destroy(Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
--- src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
@@ -1,11 +1,21 @@
 package com.zipcodewilmington.bakery.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
+@Entity
 public class Muffin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
     private String flavor;
 
     public Muffin() {
